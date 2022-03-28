8f12749ad512fa9b18d8b6108e36da1743da4f40
Nazar Tsykhuliak <nazartsykhuliak@gmail.com>
2022-03-28 19:18:42 +0300

    Add new line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello world");
+        int x = 50;
+        System.out.println(x);
     }
 }
